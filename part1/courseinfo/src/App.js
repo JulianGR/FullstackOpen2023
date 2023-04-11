bd214befc8c81cc6e67bb958f15d0b81e7da3bfb
Julián Gómez <juliangr505@gmail.com>
2023-04-11 16:49:24 +0200

    exercise 1.2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,23 +1,35 @@
 
 const Header = (props) => {
-  
+
   return (
     <h1>{props.course}</h1>
   )
 }
 
-const Content = (props) => {
-  
+const Part = (props) => {
+
 
   return (
     <p>
-        {props.part} {props.exercises}
+      {props.part} {props.exercises}
     </p>
   )
 }
 
+const Content = (props) => {
+
+
+  return (
+    <div>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
+    </div>
+  )
+}
+
 const Total = (props) => {
-  
+
   console.log('TEST')
   return (
     <p>Number of exercises {props.exercises}</p>
@@ -37,13 +49,22 @@ const App = () => {
 
   return (
     <div>
-      
-      <Header course={course}/>
-      <Content part={part1} exercises={exercises1}/>
-      <Content part={part2} exercises={exercises2}/>
-      <Content part={part3} exercises={exercises3}/>
-      <Total exercises={exercises1+ exercises2 + exercises3}/>
-            
+
+      <Header course={course} />
+      <Content
+        part1={part1}
+        part2={part2}
+        part3={part3}
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
+      <Total
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
+
     </div>
   )
 }
