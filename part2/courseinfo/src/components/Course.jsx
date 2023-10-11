e3a981164b3b72de9779e863dd60bda22484b996
Julián Gómez <juliangr505@gmail.com>
2023-10-11 16:41:00 +0200

    2.2 and 2.3

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -2,13 +2,14 @@ import Part from './Part'
 
 const Course = ({ course }) => {
     const eachPart = course.parts.map((i) => (<Part key={i.id} part={i} />))
-
+    const totalExercises = course.parts.reduce((sum, part) => sum+ part.exercises,0)
     return (
         <>
             <h1 key={course.id}>{course.name}</h1>
             <ul>
                 {eachPart}
             </ul>
+            <b>Total exercises: {totalExercises}</b>
         </>
     )
 }
