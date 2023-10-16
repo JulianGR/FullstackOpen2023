a4a40f6f65c2bbec43bb86c46e25b060d333b71d
Julián Gómez <juliangr505@gmail.com>
2023-10-16 15:29:15 +0200

    2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,9 +2,10 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', phoneNumber: '666' }
   ])
   const [newName, setNewName] = useState('')
+  const [newPhoneNumber, setNewPhoneNumber] = useState('')
 
 
 
@@ -17,7 +18,8 @@ const App = () => {
     event.preventDefault()
 
     const personObject = {
-      name: newName
+      name: newName,
+      phoneNumber: newPhoneNumber
     }
 
 
@@ -28,13 +30,17 @@ const App = () => {
     }
 
     setNewName('')
+    setNewPhoneNumber('')
   }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
 
-  const personsToShow = persons
+  const handlePhoneChange = (event) => {
+    setNewPhoneNumber(event.target.value)
+  }
+
 
   return (
     <div>
@@ -45,13 +51,17 @@ const App = () => {
             onChange={handleNameChange} />
         </div>
         <div>
+          phone number: <input value={newPhoneNumber}
+            onChange={handlePhoneChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
-        {personsToShow.map(i => (
-          <li key={i.name}>{i.name}</li>
+        {persons.map(i => (
+          <li key={i.name}>{i.name} - {i.phoneNumber}  </li>
 
         ))}
       </ul>
